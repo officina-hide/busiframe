9ef0ba9f68666a673d1f2dea039390319967d8ca
KACOMSAD\ueno <hideo@kacoms.co.jp>
2025-03-19 07:08:46 +0900

    表示情報 : 受注照会初期登録

--- src/main/java/busiframe/order/generate/CreateOrderSystem01.java
+++ src/main/java/busiframe/order/generate/CreateOrderSystem01.java
@@ -49,6 +49,8 @@ public class CreateOrderSystem01 implements BaseDisplay {
 		disp.addDispDetailData(env, DISPLAY_ID_ORDER_LIST_01, I_Order01.COL_NAME_PRODUCT_NAME, 3, "商品名","string");
 		disp.addDispDetailData(env, DISPLAY_ID_ORDER_LIST_01, I_Order01.COL_NAME_ORDER_NUMBER, 4, "受注数", "number");
 		disp.addDispDetailData(env, DISPLAY_ID_ORDER_LIST_01, I_Order01.COL_NAME_ORDER_AMOUNT, 4, "受注金額", "number");
+		// 受注照会 Lv.01画面
+		disp.addDispData(env, DISPLAY_ID_ORDER_DISP_01, DISPLAY_CD_ORDER_DISP_01, DISPLAY_TITLE_ORDER_DISP_01);
 		
 		// 受注情報初期登録 Addition 2025/03/03
 		// TODO 将来的にはJSON又はCSVで出来るようにしていく。 2025/03/03
